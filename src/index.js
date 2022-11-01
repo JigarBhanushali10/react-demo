5fd7bf007add14b2d5f9f328ac4786460794028a
JigarBhanushali10 <jigard.bhanushali12@gmail.com>
2022-11-01 18:34:23 +0530

    feat: installed bootstrap with folder Structure

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import './assets/styles/main.scss'
+// src\assets\styles\main.scss
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
